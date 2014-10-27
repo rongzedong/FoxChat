ca6c6b851cd5004c84a6cc5c90dc62d69a65aba4
wangyeming <wangyeming@dotide.com>
2014-10-28 01:58:17 +0800

    获取联系人和手机号码

---
+++ app/src/main/java/com/wangyeming/Contact/GetContactsInfo.java
@@ -0,0 +1,7 @@
+package com.wangyeming.Contact;
+
+/**
+ * Created by Wang on 2014/10/23.
+ */
+public class GetContactsInfo {
+}
--- app/src/main/java/com/wangyeming/foxchat/LineActivity.java
+++ app/src/main/java/com/wangyeming/foxchat/LineActivity.java
@@ -1,17 +1,41 @@
 package com.wangyeming.foxchat;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static android.provider.ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+
 
 public class LineActivity extends Activity {
+
+    private static final String[] PHONES_PROJECTION = new String[] {
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, //display_name
+            ContactsContract.CommonDataKinds.Phone.NUMBER,
+            ContactsContract.CommonDataKinds.Photo.PHOTO_ID,
+            ContactsContract.CommonDataKinds.Phone.CONTACT_ID };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_line);
+        System.out.println("start");
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run(){
+                System.out.println("thread start!");
+                try {
+                    getPhoneContacts();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(runnable).start();
     }
 
 
@@ -32,5 +56,29 @@ public class LineActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**得到手机通讯录联系人信息**/
+    public void getPhoneContacts() {
+        //String string = "";
+        ContentResolver cr = getContentResolver();//得到ContentResolver对象
+        Cursor cursorID = cr.query(CONTENT_URI, PHONES_PROJECTION, null, null, null);// 设置联系人光标
+        while(cursorID.moveToNext()){
+            String string = "";
+            int nameFieldColumnIndex = cursorID.getColumnIndex(PHONES_PROJECTION[0]);//返回display_name对应列的index--0
+            String contact = cursorID.getString(nameFieldColumnIndex);//获取联系人姓名
+            int index = cursorID.getColumnIndex(PHONES_PROJECTION[3]);//通过_id获取index值-- -1
+            String ContactId = cursorID.getString(index);//获取联系人对应的ID号
+            Cursor phone = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + ContactId, null, null);//设置手机号光标
+            while(phone.moveToNext()){
+                String Number = phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                string += (contact + ":" + Number + " ");
+            }
+            System.out.println(string);
+            System.out.println("--------------------------");
+            phone.close();
+        }
+        cursorID.close();
     }
 }
